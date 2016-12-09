ffb6397c15dcdd3ba2b41528aae082125bcec1c0
gemoraga <gemoraga@uc.cl>
2016-12-09 10:39:30 -0300

    link a la turbina

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 	get 'empresa', to: 'static_pages#enterprise'
 	get 'contacto', to: 'static_pages#contact'
 	get 'proyectos', to: 'static_pages#proyects'
+	get 'proyectos/turbina', to: 'projects#turbina'
 
 	root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
