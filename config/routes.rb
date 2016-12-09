79d53565dac6ccc8088945414f148f77e78cb465
jfcumsille <jfcumsille@uc.cl>
2016-12-09 16:33:26 -0300

    Translation

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+	scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+		get 'empresa', to: 'static_pages#enterprise'
+		get 'contacto', to: 'static_pages#contact'
+		get 'proyectos', to: 'static_pages#proyects'
+		get 'proyectos/turbina', to: 'projects#turbina'
 
-	get 'empresa', to: 'static_pages#enterprise'
-	get 'contacto', to: 'static_pages#contact'
-	get 'proyectos', to: 'static_pages#proyects'
-	get 'proyectos/turbina', to: 'projects#turbina'
-
-	root 'static_pages#home'
+		root 'static_pages#home'
+	end	
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
