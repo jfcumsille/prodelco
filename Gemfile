3233f6fb2fea4fb973d1e94e5ca65fda3bc6c1ec
gemoraga <gemoraga@uc.cl>
2016-12-12 15:20:27 -0300

    insertar google maps

--- Gemfile
+++ Gemfile
@@ -10,7 +10,8 @@ gem 'jquery-rails'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 
-
+gem 'gmaps4rails'
+gem 'underscore-rails'
 
 gem 'bootstrap-sass', '3.3.6'
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 	scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
 		get 'empresa', to: 'static_pages#enterprise'
 		get 'contacto', to: 'static_pages#contact'
-		get 'proyectos', to: 'static_pages#proyects'
+		get 'proyectos', to: 'static_pages#projects'
+		get 'map', to: 'static_pages#map'
 		get 'proyectos/turbina', to: 'projects#turbina'
 		post 'postContact', to: 'mails#recibirMail'
 
